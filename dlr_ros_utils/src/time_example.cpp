92105b96ebd683105435c8b1d259e621b28dc160
Mattia Leonori <mattialeonori26@gmail.com>
2023-01-11 23:52:16 +0100

    Fixed time example node

--- dlr_ros_utils/src/time_example.cpp
+++ dlr_ros_utils/src/time_example.cpp
@@ -4,6 +4,8 @@ int main (int argc, char** argv)
 {
     ros::init(argc, argv, "time_example");
 
+    ros::NodeHandle nh;
+
     ros::Time start_time = ros::Time::now();
 
     ros::Rate loop_rate(0.5);
@@ -11,7 +13,6 @@ int main (int argc, char** argv)
     while(ros::ok())
     {
         ROS_INFO_STREAM("Actual time: " << ros::Time::now());
-        ROS_INFO_STREAM("Actual time in seconds: " << ros::Time::now().toSec());
         ROS_INFO_STREAM("Elapsed time from start_time in seconds: " << (ros::Time::now() - start_time).toSec() << "\n");
         
         loop_rate.sleep();
