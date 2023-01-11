e7c13ffac098c991ef6ad530ab97920091d24bb2
Mattia Leonori <mattialeonori26@gmail.com>
2023-01-11 23:47:02 +0100

    Added ros cpp time example

---
+++ dlr_ros_utils/src/time_example.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+
+int main (int argc, char** argv)
+{
+    ros::init(argc, argv, "time_example");
+
+    ros::Time start_time = ros::Time::now();
+
+    ros::Rate loop_rate(0.5);
+
+    while(ros::ok())
+    {
+        ROS_INFO_STREAM("Actual time: " << ros::Time::now());
+        ROS_INFO_STREAM("Actual time in seconds: " << ros::Time::now().toSec());
+        ROS_INFO_STREAM("Elapsed time from start_time in seconds: " << (ros::Time::now() - start_time).toSec() << "\n");
+        
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
